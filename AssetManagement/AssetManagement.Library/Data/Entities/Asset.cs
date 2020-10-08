39527ee7956f876646a6f3bb94ab267074af0e05
mizan <mizan.441980@gmail.com>
2020-10-08 10:23:04 +0600

    Department Entity Added

---
+++ AssetManagement/AssetManagement.Library/Data/Entities/Asset.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetManagement.Library.Data.Entities
+{
+    public class Asset : EntityBase<Guid>
+    {
+        public string AssetName { get; set; }
+        public string AssetCode { get; set; }
+        public double AssetPrice { get; set; }
+        public int AssetQuantity { get; set; }
+        public DateTime AssetPurchaseDate { get; set; }
+        public DateTime? AssetExpirationDate { get; set; }
+        public string ShopName { get; set; }
+        public string ShopAddress { get; set; }
+        public bool IsActive { get; set; }
+        public AllotedStatus AllotedStatus { get; set; }
+        public Guid AssetCategoryId { get; set; }
+
+        public virtual AssetCategory AssetCategory { get; set; }
+    }
+
+    public enum AllotedStatus
+    {
+        None = 0, Assign, Retain, Unavailable
+    }
+}
---
+++ AssetManagement/AssetManagement.Library/Data/Entities/AssetAllotment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetManagement.Library.Data.Entities
+{
+    public class AssetAllotment : EntityBase<Guid>
+    {
+        public string AssetName { get; set; }
+        public string AssetAllotedTo { get; set; }
+        public string AssetDisAllotedIssue { get; set; }
+        public Guid EmployeeId { get; set; }
+        public DateTime AssetAllotedDate { get; set; }
+        public DateTime? AssetDisAllotedDate { get; set; }
+        public Guid AssetId { get; set; }
+        public virtual Asset Asset { get; set; }
+    }
+}
